a124cac6b8316b30859bd4ac652957ceb34f96b2
hwrdyen <hwrdpvt@gmail.com>
2023-12-30 01:21:38 -0500

    Completed first draft of WarehouseHome Page Styling

--- client/src/components/Card/Warehouse/WarehouseCard.jsx
+++ client/src/components/Card/Warehouse/WarehouseCard.jsx
@@ -1,3 +1,4 @@
+import "./WarehouseCard.scss";
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
@@ -7,26 +8,99 @@ import WarehouseModal from "../../Modal/Warehouse/WarehouseModal";
 // Assets
 import EditIcon from "../../../assets/Icons/edit-24px.svg";
 import DeleteIcon from "../../../assets/Icons/delete_outline-24px.svg";
+import ArrowRight from "../../../assets/Icons/chevron_right-24px.svg";
 
 function WarehouseCard({ SingleWarehouseInfo, setUpdateWarehouseData }) {
   const [ShowModal, setShowModal] = useState(false);
 
   return (
-    <>
-      <div>This is Warehouse Card:</div>
+    <div className="WarehouseCard__Container">
+      {/* Warehouse Detail */}
+      <div className="WarehouseCard__Detail">
+        {/* Name & Address */}
+        <div className="WarehouseCard__Detail--section">
+          {/* Name */}
+          <div className="WarehouseCard__Detail--sectionblock">
+            <div className="WarehouseCard__Detail--blocktitle">WAREHOUSE</div>
+            <NavLink
+              to={`/warehouse/${SingleWarehouseInfo?._id}`}
+              className="WarehouseCard__Detail--namelink"
+            >
+              <div className="WarehouseCard__Detail--nameblock">
+                <div className="WarehouseCard__Detail--info WarehouseCard__Detail--nametitle">
+                  {SingleWarehouseInfo?.name}
+                </div>
+                <img
+                  src={ArrowRight}
+                  alt="Right Arrow"
+                  className="WarehouseCard__Detail--namearrow"
+                />
+              </div>
+            </NavLink>
+          </div>
 
-      <NavLink to={`/warehouse/${SingleWarehouseInfo?._id}`}>
-        <div>{SingleWarehouseInfo?.name}</div>
-      </NavLink>
+          {/* Address */}
+          <div className="WarehouseCard__Detail--sectionblock">
+            <div className="WarehouseCard__Detail--blocktitle">ADDRESS</div>
+            <div className="WarehouseCard__Detail--addressblock">
+              <div className="WarehouseCard__Detail--info">
+                {SingleWarehouseInfo?.address}
+              </div>
+              <div className="WarehouseCard__Detail--info">
+                {SingleWarehouseInfo?.city}, {SingleWarehouseInfo?.country}
+              </div>
+            </div>
+          </div>
+        </div>
 
-      <img
-        src={DeleteIcon}
-        alt="Delete Icon"
-        onClick={() => setShowModal(true)}
-      />
-      <NavLink to={`/warehouse/edit/${SingleWarehouseInfo?._id}`}>
-        <img src={EditIcon} alt="Edit Icon" />
-      </NavLink>
+        {/* Contact Name & Contact Info */}
+        <div className="WarehouseCard__Detail--section">
+          {/* Contact Name */}
+          <div className="WarehouseCard__Detail--sectionblock">
+            <div className="WarehouseCard__Detail--blocktitle">
+              CONTACT NAME
+            </div>
+            <div className="WarehouseCard__Detail--info">
+              {SingleWarehouseInfo?.contact?.name}
+            </div>
+          </div>
+
+          {/* Contact Info */}
+          <div className="WarehouseCard__Detail--sectionblock">
+            <div className="WarehouseCard__Detail--blocktitle">
+              CONTACT INFORMATION
+            </div>
+            <div className="WarehouseCard__Detail--contactinfoblock">
+              <div className="WarehouseCard__Detail--info">
+                {SingleWarehouseInfo?.contact?.phone}
+              </div>
+              <div className="WarehouseCard__Detail--info">
+                {SingleWarehouseInfo?.contact?.email}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Button */}
+      <div className="WarehouseCard__ButtonContainer">
+        <img
+          src={DeleteIcon}
+          alt="Delete Icon"
+          onClick={() => setShowModal(true)}
+          className="WarehouseCard__Button--delete"
+        />
+        <NavLink
+          to={`/warehouse/edit/${SingleWarehouseInfo?._id}`}
+          className="WarehouseCard__Button--editlink"
+        >
+          <img
+            src={EditIcon}
+            alt="Edit Icon"
+            className="WarehouseCard__Button--edit"
+          />
+        </NavLink>
+      </div>
 
       {ShowModal && (
         <WarehouseModal
@@ -35,7 +109,7 @@ function WarehouseCard({ SingleWarehouseInfo, setUpdateWarehouseData }) {
           onClose={() => setShowModal(false)}
         />
       )}
-    </>
+    </div>
   );
 }
 
--- client/src/components/CardList/Warehouse/WarehouseCardList.jsx
+++ client/src/components/CardList/Warehouse/WarehouseCardList.jsx
@@ -1,12 +1,56 @@
 import "./WarehouseCardList.scss";
-import { NavLink } from "react-router-dom";
 
 // Component
 import WarehouseCard from "../../Card/Warehouse/WarehouseCard";
 
+// Assets
+import SortIcon from "../../../assets/Icons/sort-24px.svg";
+
 function WarehouseCardList({ AllWarehouseInfo, setUpdateWarehouseData }) {
   return (
     <div className="CardList__Container">
+      <div className="CardList__TabletTitle">
+        <div className="CardList__TabletTitle--title">
+          <div className="CardList__TabletTitle--blocktitlesection">
+            <div className="CardList__TabletTitle--blocktitle">
+              WAREHOUSE{" "}
+              <img
+                src={SortIcon}
+                alt="Sort Icon"
+                className="CardList__TabletTitle--sorticon"
+              />
+            </div>
+            <div className="CardList__TabletTitle--blocktitle">
+              ADDRESS{" "}
+              <img
+                src={SortIcon}
+                alt="Sort Icon"
+                className="CardList__TabletTitle--sorticon"
+              />
+            </div>
+          </div>
+          <div className="CardList__TabletTitle--blocktitlesection">
+            <div className="CardList__TabletTitle--blocktitle">
+              CONTACT NAME{" "}
+              <img
+                src={SortIcon}
+                alt="Sort Icon"
+                className="CardList__TabletTitle--sorticon"
+              />
+            </div>
+            <div className="CardList__TabletTitle--blocktitle">
+              CONTACT INFORMATION{" "}
+              <img
+                src={SortIcon}
+                alt="Sort Icon"
+                className="CardList__TabletTitle--sorticon"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="CardList__TabletTitle--button">ACTIONS</div>
+      </div>
       <div>
         {AllWarehouseInfo.map((SingleWarehouseInfo) => (
           <WarehouseCard
