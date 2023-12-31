7568ddccb0e55ee31a8fe4878e671ab6608a460b
hwrdyen <hwrdpvt@gmail.com>
2023-12-30 19:51:14 -0500

    Completed first draft of WarehouseDetail Info section styling

--- client/src/components/CardList/Warehouse/WarehouseInventoryCardList/WarehouseInventoryCardList.jsx
+++ client/src/components/CardList/Warehouse/WarehouseInventoryCardList/WarehouseInventoryCardList.jsx
@@ -1,14 +1,69 @@
+import "./WarehouseInventoryCardList.scss";
+
+// Components
 import WarehouseInventoryCard from "../../../Card/Warehouse/WarehouseInventoryCard/WarehouseInventoryCard";
 
-import { useEffect, useState } from "react";
-import axios from "axios";
+// Assets
+import SortIcon from "../../../../assets/Icons/sort-24px.svg";
+
 function WarehouseInventoryCardList({
   WarehouseInventoriesInfo,
   setUpdateWarehouseInventoriesData,
 }) {
   return (
-    <>
-      <div> This is InventoryList inside WarehouseDetail: </div>
+    <div className="WarehouseInventoryCardList__Container">
+      <div className="WarehouseInventoryCardList__TabletTitle">
+        <div className="WarehouseInventoryCardList__TabletTitle--title">
+          <div className="WarehouseInventoryCardList__TabletTitle--blocktitlesection">
+            <div className="WarehouseInventoryCardList__TabletTitle--blocktitle">
+              INVENTORY ITEM{" "}
+              <img
+                src={SortIcon}
+                alt="Sort Icon"
+                className="WarehouseInventoryCardList__TabletTitle--sorticon"
+              />
+            </div>
+            <div className="WarehouseInventoryCardList__TabletTitle--blocktitle">
+              CATEGORY{" "}
+              <img
+                src={SortIcon}
+                alt="Sort Icon"
+                className="WarehouseInventoryCardList__TabletTitle--sorticon"
+              />
+            </div>
+          </div>
+          <div className="WarehouseInventoryCardList__TabletTitle--blocktitlesection">
+            <div className="WarehouseInventoryCardList__TabletTitle--blocktitle">
+              STATUS{" "}
+              <img
+                src={SortIcon}
+                alt="Sort Icon"
+                className="WarehouseInventoryCardList__TabletTitle--sorticon"
+              />
+            </div>
+            <div className="WarehouseInventoryCardList__TabletTitle--blocktitle">
+              QTY{" "}
+              <img
+                src={SortIcon}
+                alt="Sort Icon"
+                className="WarehouseInventoryCardList__TabletTitle--sorticon"
+              />
+            </div>
+            <div className="WarehouseInventoryCardList__TabletTitle--blocktitle">
+              WAREHOUSE{" "}
+              <img
+                src={SortIcon}
+                alt="Sort Icon"
+                className="WarehouseInventoryCardList__TabletTitle--sorticon"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="WarehouseInventoryCardList__TabletTitle--button">
+          ACTIONS
+        </div>
+      </div>
       <div>
         {WarehouseInventoriesInfo.map((WarehouseInventoryInfo) => (
           <WarehouseInventoryCard
@@ -20,7 +75,7 @@ function WarehouseInventoryCardList({
           />
         ))}
       </div>
-    </>
+    </div>
   );
 }
 
--- client/src/pages/Warehouse/WarehouseDetail.jsx
+++ client/src/pages/Warehouse/WarehouseDetail.jsx
@@ -1,3 +1,4 @@
+import "./WarehouseDetail.scss";
 import { useState, useEffect } from "react";
 import { NavLink, useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
@@ -11,6 +12,7 @@ import WarehouseInventoryCardList from "../../components/CardList/Warehouse/Ware
 
 // Assets
 import ArrowBack from "../../assets/Icons/arrow_back-24px.svg";
+import EditIcon from "../../assets/Icons/edit-24px.svg";
 
 function WarehouseDetail() {
   const { warehouseID } = useParams();
@@ -55,37 +57,97 @@ function WarehouseDetail() {
   return (
     <>
       <NavBar />
-      <img
-        onClick={(e) => {
-          e.preventDefault();
-          navigate("/warehouse");
-        }}
-        src={ArrowBack}
-        alt="Back Arrow"
-      />
-
-      <div>This is WarehouseDetail for {warehouseID}</div>
-      <div>Warehouse Info: </div>
       <div>
         {Loading ? (
           <Spinner />
         ) : (
-          <>
-            <div>Warehouse Name: {SingleWarehouseInfo?.name}</div>
-            <NavLink to={`/warehouse/edit/${SingleWarehouseInfo?._id}`}>
-              Edit
-            </NavLink>
-            <div>Warehouse Address: {SingleWarehouseInfo?.address}</div>
-            <div>
-              Contact Name: {SingleWarehouseInfo?.contact?.name}{" "}
-              {SingleWarehouseInfo?.contact?.position}
-            </div>
-            <div>
-              Contact Information: {SingleWarehouseInfo?.contact?.phone}{" "}
-              {SingleWarehouseInfo?.contact?.email}
+          <div className="WarehouseDetail__Container">
+            <div className="WarehouseDetail__TitleInfo">
+              {/* Warehouse Detail Title */}
+              <div className="WarehouseDetail__TitleContainer">
+                <img
+                  onClick={(e) => {
+                    e.preventDefault();
+                    navigate("/warehouse");
+                  }}
+                  src={ArrowBack}
+                  alt="Back Arrow"
+                  className="WarehouseDetail__TitleContainer--backicon"
+                />
+                <div className="WarehouseDetail__TitleContainer--title">
+                  {SingleWarehouseInfo?.name}
+                </div>
+                <NavLink
+                  to={`/warehouse/edit/${SingleWarehouseInfo?._id}`}
+                  className="WarehouseDetail__TitleContainer--editlink"
+                >
+                  <div className="WarehouseDetail__TitleContainer--editblock">
+                    <img
+                      src={EditIcon}
+                      alt="Edit Icon"
+                      className="WarehouseDetail__TitleContainer--editicon"
+                    />
+                    <div className="WarehouseDetail__TitleContainer--edittitle">
+                      Edit
+                    </div>
+                  </div>
+                </NavLink>
+              </div>
+
+              {/* Warehouse Detail Info */}
+              <div className="WarehouseDetail__InfoContainer">
+                {/* Warehouse Address */}
+                <div className="WarehouseDetail__InfoContainer--addresssection">
+                  <div className="WarehouseDetail__InfoContainer--addresstitle">
+                    WAREHOUSE ADDRESS:
+                  </div>
+                  <div className="WarehouseDetail__InfoContainer--addresscontent">
+                    <div className="WarehouseDetail__InfoContainer--addresscontentaddress">
+                      {SingleWarehouseInfo?.address},&#160;
+                    </div>
+                    <div className="WarehouseDetail__InfoContainer--addresscontentcitycountry">
+                      {SingleWarehouseInfo?.city},&#160;
+                      {SingleWarehouseInfo?.country}
+                    </div>
+                  </div>
+                </div>
+
+                {/* Contact Name & Contact Information */}
+                <div className="WarehouseDetail__InfoContainer--contactsection">
+                  {/* Contact Name */}
+                  <div className="WarehouseDetail__InfoContainer--contactnamesection">
+                    <div className="WarehouseDetail__InfoContainer--contactnametitle">
+                      CONTACT NAME:
+                    </div>
+                    <div className="WarehouseDetail__InfoContainer--contactnameblock">
+                      <div className="WarehouseDetail__InfoContainer--contactnamecontent">
+                        {SingleWarehouseInfo?.contact?.name}
+                      </div>
+                      <div className="WarehouseDetail__InfoContainer--contactpositioncontent">
+                        {SingleWarehouseInfo?.contact?.position}
+                      </div>
+                    </div>
+                  </div>
+
+                  {/* Contact Information */}
+                  <div className="WarehouseDetail__InfoContainer--contactinfosection">
+                    <div className="WarehouseDetail__InfoContainer--contactinfotitle">
+                      CONTACT INFORMATION:
+                    </div>
+                    <div className="WarehouseDetail__InfoContainer--contactinfoblock">
+                      <div className="WarehouseDetail__InfoContainer--contactphonecontent">
+                        {SingleWarehouseInfo?.contact?.phone}
+                      </div>
+                      <div className="WarehouseDetail__InfoContainer--contactemailcontent">
+                        {SingleWarehouseInfo?.contact?.email}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
 
-            <div>InventoryList:</div>
+            {/* Warehouse-Inventory CardList */}
             <div>
               <WarehouseInventoryCardList
                 WarehouseInventoriesInfo={WarehouseInventoriesInfo}
@@ -94,7 +156,7 @@ function WarehouseDetail() {
                 }
               />
             </div>
-          </>
+          </div>
         )}
       </div>
 
