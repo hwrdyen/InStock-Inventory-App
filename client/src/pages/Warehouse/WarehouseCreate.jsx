0691e0ab47c8c0a48b6afdcc7eb176acb2b87033
hwrdyen <hwrdpvt@gmail.com>
2023-12-30 20:59:11 -0500

    Completed first draft of WarehouseCreate page styling

--- client/src/pages/Warehouse/WarehouseCreate.jsx
+++ client/src/pages/Warehouse/WarehouseCreate.jsx
@@ -1,3 +1,4 @@
+import "./WarehouseCreate.scss";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
@@ -56,102 +57,216 @@ function WarehouseCreate() {
   return (
     <>
       <NavBar />
-      <img
-        onClick={(e) => {
-          e.preventDefault();
-          navigate("/warehouse");
-        }}
-        src={ArrowBack}
-        alt="Back Arrow"
-      />
-      <div>Add New Warehouse</div>
+      <div className="WarehouseCreate__Contianer">
+        {/* WarehouseCreate Title */}
+        <div className="WarehouseCreate__TitleContainer">
+          <img
+            onClick={(e) => {
+              e.preventDefault();
+              navigate("/warehouse");
+            }}
+            src={ArrowBack}
+            alt="Back Arrow"
+            className="WarehouseCreate__TitleContainer--backicon"
+          />
+          <div className="WarehouseCreate__TitleContainer--title">
+            Add New Warehouse
+          </div>
+        </div>
+
+        {/* Warehouse Create Form  */}
+        <div className="WarehouseCreate__InputFormContainer">
+          {/* Form Input Container */}
+          <div className="WarehouseCreate__FormContainer">
+            {/* Warehouse Details */}
+            <div className="WarehouseCreate__FormContainer--warehousedetailsection">
+              <div className="WarehouseCreate__FormContainer--detailtitle">
+                Warehouse Details
+              </div>
+
+              {/* Warehouse Name */}
+              <div className="WarehouseCreate__FormContainer--inputblock">
+                <label
+                  htmlFor="warehouseName"
+                  className="WarehouseCreate__FormContainer--detaillabel"
+                >
+                  Warehouse Name
+                </label>
+                <input
+                  type="text"
+                  id="warehouseName"
+                  placeholder="Warehouse Name"
+                  value={WarehouseName}
+                  onChange={(e) => setWarehouseName(e.target.value)}
+                  required
+                  htmlFor="warehouseName"
+                  className="WarehouseCreate__FormContainer--detailinput"
+                />
+              </div>
+
+              {/* Warehouse Address */}
+              <div className="WarehouseCreate__FormContainer--inputblock">
+                <label
+                  htmlFor="warehouseAddress"
+                  className="WarehouseCreate__FormContainer--detaillabel"
+                >
+                  Street Address
+                </label>
+                <input
+                  type="text"
+                  id="warehouseAddress"
+                  placeholder="Street Address"
+                  value={WarehouseAddress}
+                  onChange={(e) => setWarehouseAddress(e.target.value)}
+                  required
+                  className="WarehouseCreate__FormContainer--detailinput"
+                />
+              </div>
+
+              {/* Warehouse City */}
+              <div className="WarehouseCreate__FormContainer--inputblock">
+                <label
+                  htmlFor="warehouseCity"
+                  className="WarehouseCreate__FormContainer--detaillabel"
+                >
+                  City
+                </label>
+                <input
+                  type="text"
+                  id="warehouseCity"
+                  placeholder="City"
+                  value={WarehouseCity}
+                  onChange={(e) => setWarehouseCity(e.target.value)}
+                  required
+                  className="WarehouseCreate__FormContainer--detailinput"
+                />
+              </div>
+
+              {/* Warehouse Country */}
+              <div className="WarehouseCreate__FormContainer--inputblock">
+                <label
+                  htmlFor="warehouseCountry"
+                  className="WarehouseCreate__FormContainer--detaillabel"
+                >
+                  Country
+                </label>
+                <input
+                  type="text"
+                  id="warehouseCountry"
+                  placeholder="Country"
+                  value={WarehouseCountry}
+                  onChange={(e) => setWarehouseCountry(e.target.value)}
+                  required
+                  className="WarehouseCreate__FormContainer--detailinput"
+                />
+              </div>
+            </div>
+
+            {/* Contact Details */}
+            <div className="WarehouseCreate__FormContainer--contactdetailsection">
+              <div className="WarehouseCreate__FormContainer--detailtitle">
+                Contact Details
+              </div>
+
+              {/* Contact Name */}
+              <div className="WarehouseCreate__FormContainer--inputblock">
+                <label
+                  htmlFor="contactName"
+                  className="WarehouseCreate__FormContainer--detaillabel"
+                >
+                  Contact Name
+                </label>
+                <input
+                  type="text"
+                  id="contactName"
+                  placeholder="Contact Name"
+                  value={ContactName}
+                  onChange={(e) => setContactName(e.target.value)}
+                  required
+                  className="WarehouseCreate__FormContainer--detailinput"
+                />
+              </div>
+
+              {/* Contact Position */}
+              <div className="WarehouseCreate__FormContainer--inputblock">
+                <label
+                  htmlFor="contactPosition"
+                  className="WarehouseCreate__FormContainer--detaillabel"
+                >
+                  Position
+                </label>
+                <input
+                  type="text"
+                  id="contactPosition"
+                  placeholder="Position"
+                  value={ContactPosition}
+                  onChange={(e) => setContactPosition(e.target.value)}
+                  required
+                  className="WarehouseCreate__FormContainer--detailinput"
+                />
+              </div>
+
+              {/* Contact Phone Number */}
+              <div className="WarehouseCreate__FormContainer--inputblock">
+                <label
+                  htmlFor="contactPhone"
+                  className="WarehouseCreate__FormContainer--detaillabel"
+                >
+                  Phone Number
+                </label>
+                <input
+                  type="text"
+                  id="contactPhone"
+                  placeholder="Phone Number"
+                  value={ContactPhone}
+                  onChange={(e) => setContactPhone(e.target.value)}
+                  required
+                  className="WarehouseCreate__FormContainer--detailinput"
+                />
+              </div>
+
+              {/* Contact Phone Email */}
+              <div className="WarehouseCreate__FormContainer--inputblock">
+                <label
+                  htmlFor="contactEmail"
+                  className="WarehouseCreate__FormContainer--detaillabel"
+                >
+                  Email
+                </label>
+                <input
+                  type="text"
+                  id="contactEmail"
+                  placeholder="Email"
+                  value={ContactEmail}
+                  onChange={(e) => setContactEmail(e.target.value)}
+                  required
+                  className="WarehouseCreate__FormContainer--detailinput"
+                />
+              </div>
+            </div>
+          </div>
+
+          {/* Button Input Container */}
+          <div className="WarehouseCreate__ButtonContainer">
+            <button
+              onClick={() => navigate("/warehouse")}
+              className="WarehouseCreate__ButtonContainer--cancelbutton"
+            >
+              Cancel
+            </button>
+            <button
+              onClick={handleCreateWarehouse}
+              className="WarehouseCreate__ButtonContainer--createbutton"
+            >
+              + Add New Warehouse
+            </button>
+          </div>
+        </div>
+      </div>
 
       {/* Warehouse Details */}
-      <div>Warehouse Details</div>
-      <label htmlFor="warehouseName">Warehouse Name</label>
-      <input
-        type="text"
-        id="warehouseName"
-        placeholder="Warehouse Name"
-        value={WarehouseName}
-        onChange={(e) => setWarehouseName(e.target.value)}
-        required
-      />
-
-      <label htmlFor="warehouseAddress">Street Address</label>
-      <input
-        type="text"
-        id="warehouseAddress"
-        placeholder="Street Address"
-        value={WarehouseAddress}
-        onChange={(e) => setWarehouseAddress(e.target.value)}
-        required
-      />
-
-      <label htmlFor="warehouseCity">City</label>
-      <input
-        type="text"
-        id="warehouseCity"
-        placeholder="City"
-        value={WarehouseCity}
-        onChange={(e) => setWarehouseCity(e.target.value)}
-        required
-      />
-
-      <label htmlFor="warehouseCountry">Country</label>
-      <input
-        type="text"
-        id="warehouseCountry"
-        placeholder="Country"
-        value={WarehouseCountry}
-        onChange={(e) => setWarehouseCountry(e.target.value)}
-        required
-      />
 
       {/* Contact Details */}
-      <div>Contact Details</div>
-      <label htmlFor="contactName">Contact Name</label>
-      <input
-        type="text"
-        id="contactName"
-        placeholder="Contact Name"
-        value={ContactName}
-        onChange={(e) => setContactName(e.target.value)}
-        required
-      />
-
-      <label htmlFor="contactPosition">Position</label>
-      <input
-        type="text"
-        id="contactPosition"
-        placeholder="Position"
-        value={ContactPosition}
-        onChange={(e) => setContactPosition(e.target.value)}
-        required
-      />
-
-      <label htmlFor="contactPhone">Phone Number</label>
-      <input
-        type="text"
-        id="contactPhone"
-        placeholder="Phone Number"
-        value={ContactPhone}
-        onChange={(e) => setContactPhone(e.target.value)}
-        required
-      />
-
-      <label htmlFor="contactEmail">Email</label>
-      <input
-        type="text"
-        id="contactEmail"
-        placeholder="Email"
-        value={ContactEmail}
-        onChange={(e) => setContactEmail(e.target.value)}
-        required
-      />
-
-      <button onClick={() => navigate("/warehouse")}>Cancel</button>
-      <button onClick={handleCreateWarehouse}>+ Add New Warehouse</button>
 
       <Footer />
     </>
